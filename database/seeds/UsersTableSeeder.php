31f8f46faa2c8f309ae2b9e70eac61a409655db1
Thiago Marquesini <contato@marquesini.com.br>
2018-05-23 09:38:05 -0300

    Initial commit

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'Administrator',
+            'email' => 'admin@user.com',
+            'password' => bcrypt('secret'),
+            'phone' => '0',
+            'blocked' => false,
+            'role' => 'admin'
+        ]);
+    }
+}
